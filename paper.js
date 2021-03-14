aa2f60c8756167cfc1fea94c70ef132fc059d732
devansh2008dg8586 <80566266+devansh2008dg8586@users.noreply.github.com>
2021-03-14 12:35:31 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,27 @@
+class Paper{
+    constructor(x,y,r){
+        var options = {
+        'isStatic':false,
+        'restitution':0.3,
+        'friction':0.5,
+        'densiy':2
+        }
+        
+        this.body = Bodies.circle(x,y,r,options);
+        this.image=loadImage("paper(1).png")
+        World.add(world, this.body); 
+        }
+    display(){
+        var pos=this.body.position;
+        var angle=this.body.angle;
+        push();
+       translate(pos.x,pos.y);
+
+       rotate(angle)
+        imageMode(CENTER);
+        image(this.image,0,0,20,20);
+        pop();
+
+    }
+}
+ 
